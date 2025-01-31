6ec943bcf178816b64a37cef04c75a81f48c1f2a
Mendim10 <mendimbehluli2006@gmail.com>
2025-01-31 02:44:44 +0000

    Update UserKlienti.php

--- MainPageHTML/UserKlienti.php
+++ MainPageHTML/UserKlienti.php
@@ -1,33 +1,21 @@
 <?php
+class UserKlienti {
+    private $conn;
+    private $table_name = 'contact_messages';
 
-    class UserKlient{
-
-        private $conn;
-        private $table_name = 'contact_messages';
-
-        public function __construct($db) {
-            $this-> conn = $db;
-        }
-       
-        public function getMessages($emri, $email, $message): bool {
-            if ($password !== $confirm_password) {
-                throw new Exception("Fjalëkalimet nuk përputhen.");
-            }
-        
-            $query = "INSERT INTO {$this->table_name} (emri, email, message) VALUES (:emri, :email, :message)";
-            $stmt = $this->conn->prepare($query);
-        
-            $stmt->bindValue(':emri', $emri);
-            $stmt->bindValue(':email', $email);
-            $stmt->bindValue(':message', $message);
-        
-            if ($stmt->execute()) {
-                return true;
-            } else {
-                throw new Exception("Gabim gjatë regjistrimit.");
-            }
-        }
-
+    public function __construct($db) {
+        $this->conn = $db;
     }
 
+    public function register($emri, $email, $message): bool {
+        $query = "INSERT INTO {$this->table_name} (emri, email, message) VALUES (:emri, :email, :message)";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindValue(':emri', $emri);
+        $stmt->bindValue(':email', $email);
+        $stmt->bindValue(':message', $message);
+
+        return $stmt->execute();
+    }
+}
 ?>
