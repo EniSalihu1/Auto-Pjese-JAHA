3f380c9d01c93748eadf1afba860efa92d2aeccc
Mendim10 <mendimbehluli2006@gmail.com>
2025-03-31 01:08:42 +0100

    Update

--- MainPageHTML/createProduct.php
+++ MainPageHTML/createProduct.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shto Produkt</title>
+    <link rel="stylesheet" href="../ProduktetCss/createProduct.css"> 
+</head>
+<body>
+    
 <?php
 $servername = "localhost";
 $username = "root";
@@ -69,7 +79,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shto Produkt</title>
-    <link rel="stylesheet" href="../ProduktetCss/createProduct.css">  
+    <link rel="stylesheet" href="../ProduktetCss/createProduct.css"> 
 </head>
 <body>
 
