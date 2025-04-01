61eb29a9c6e5763cf67b7d8e1e45f4141899714b
Mendim10 <mendimbehluli2006@gmail.com>
2025-04-01 23:32:45 +0100

    header footer

--- MainPageHTML/createProduct.php
+++ MainPageHTML/createProduct.php
@@ -7,6 +7,29 @@
     <link rel="stylesheet" href="../ProduktetCss/createProduct.css"> 
 </head>
 <body>
+<header class="nav">
+
+<a href="Main.php">
+<img src="../Images/Logo.jpg" alt="Logo">
+</a>
+<ul>
+        <li><a href="Main.php">Home</a></li>
+        <li><a href="News.php">News</a></li>
+        <li><a href="Produkt.php">Products</a></li>
+        <li><a href="AboutUs.php">About Us</a></li>
+        <li><a href="Contact.php">Contact Us</a></li>
+        <?php if ($isLoggedIn && $role === 'admin'): ?>            
+            <li><a href="dashboard.php">Dashboard</a></li>          
+        <?php endif; ?>
+        <li>
+            <?php if ($isLoggedIn): ?>
+            <button><a href="logout.php" id="LogOutButton">Log Out</a></button>
+             <?php else: ?>
+            <button><a href="login.php" id="LogInButton">Log In</a></button>
+            <?php endif; ?>
+        </li>
+</ul>
+</header>
     
 <?php
 $servername = "localhost";
@@ -102,6 +125,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <button type="submit">Shto Produktin</button>
     </form>
 </div>
+<div class="footer">
+
+      <div class="footer-container">
+          <div class="footer-section">
+              <h4>Rreth Nesh</h4>
+              <p>Ne ofrojmë pjesë cilësore për çdo lloj automjeti. Garantojmë cilësi dhe besueshmëri.</p>
+          </div>
+          
+          <div class="footer-section">
+              <h4>Na Kontaktoni </h4>
+              <p>Email: info@autopjesa.com</p>
+              <p>Tel: +383 44 296 081</p>
+              <p>Adresa: Rruga Ferizajit Km 1, Gjilan</p>
+          </div>
+          <div class="footer-section social">
+              <h4>Na Ndiqni ne faqen tone ne:</h4>
+                  <div class="social-icons">
+                      <a href="https://www.facebook.com/profile.html?id=100039106436166"><img src="../Images/Facebook.webp" alt="Facebook"></a>
+                  </div>
+          </div>
+      </div>
+      <div class="footer-bottom">
+          <p>&copy; 2024 AutoPjesa. Të gjitha të drejtat e rezervuara.</p>
+      </div>
+
+  </div>
 
 </body>
 </html>
